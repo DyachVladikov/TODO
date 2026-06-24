3d28f460270800948a4c7ffd600c463693382dd2
Dyach_Vladikov <dyachvladikov08@gmail.com>
2026-06-24 15:22:47 +0300

    fixed: "frozen start"

--- server/index.js
+++ server/index.js
@@ -13,20 +13,37 @@ const app = express();
 const Port = process.env.PORT || 3002;
 const DbPassword = process.env.DB_PASSWORD;
 const DbUser = process.env.DB_USER;
+const MONGO_URI = `mongodb+srv://${DbUser}:${DbPassword}@todo.y4zsq8v.mongodb.net/TodoDB`;
 
 app.use(cors());
 app.use(express.json());
 
+let cachedDb = null;
+
+const connectDB = async () => {
+  if (cachedDb) {
+    return cachedDb;
+  }
+  // Если база спит (холодный старт) - ждем подключения
+  const db = await mongoose.connect(MONGO_URI);
+  cachedDb = db;
+  console.log("DB Connected Serverless Style");
+  return db;
+};
+
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (error) {
+    console.error("DB Connection Error: ", error);
+    res.status(500).json({ message: "Ошибка подключения к базе данных" });
+  }
+});
+
 app.use("/api", AuthRoutes);
 app.use("/api", TodoRoutes);
 app.use("/api", folderRoutes);
-
-mongoose
-  .connect(
-    `mongodb+srv://${DbUser}:${DbPassword}@todo.y4zsq8v.mongodb.net/TodoDB`,
-  )
-  .then(() => console.log("DB Connected"))
-  .catch((err) => console.log("DB Connection Error: ", err));
 
 if (process.env.NODE_ENV !== "production") {
   app.listen(Port, () => console.log(`Server running on ${Port}`));
